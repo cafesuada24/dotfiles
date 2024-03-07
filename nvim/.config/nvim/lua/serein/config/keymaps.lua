f6d23f934c9ac7d0e23814865b61f0437e1852e7
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-03-07 18:48:56 +0700

    update

--- nvim/.config/nvim/lua/serein/config/keymaps.lua
+++ nvim/.config/nvim/lua/serein/config/keymaps.lua
@@ -10,6 +10,21 @@ keymap('', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-
+-- Text moving
 keymap('v', 'J', ":m '>+1<CR>gv=gv", opts)
 keymap('v', 'K', ":m '<-2<CR>gv=gv", opts)
+
+-- General
+keymap('n', '<leader>nh', ':nohl<CR>', opts)
+
+keymap('n', 'x', '"_x', opts)
+
+keymap('n', '<leader>to', ':tabnew<CR>', opts)
+keymap('n', '<leader>tx', ':tabclose<CR>', opts)
+keymap('n', '<leader>tn', ':tabnext<CR>', opts)
+keymap('n', '<leader>tp', ':tabprev<CR>', opts)
+
+keymap('v', '<', '<gv', opts)
+keymap('v', '<', '>gv', opts)
+
+
