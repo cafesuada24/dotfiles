543de5e159d35ed9caf250a4868b0dcb8f70c562
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-09-24 14:23:08 +0700

    Update FZF plugin spec

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,13 @@
 return {
-    'junegunn/fzf',
-    lazy = true,    
+    -- 'junegunn/fzf',
+    dir = '/opt/fzf/',
+    -- lazy = true,    
     -- opts = {},
     keys = require('config.keymaps').fzf,
     -- config = function() end
 }
+
+-- return {
+--     dir = '/opt/fzf',
+--     keys = require('config.keymaps').fzf,
+-- }
