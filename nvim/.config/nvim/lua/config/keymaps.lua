207f17ec20e35b1746110156eb76c626c9df495a
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-10-02 13:49:13 +0700

    Update keymap and option

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,14 @@ local M = {}
 M.init = function()
     local map = vim.keymap.set
     
+    map('n', '<leader>nh', '<cmd>nohlsearch<CR>')
+
+        -- Window Navigation
+    map("n", "<C-h>", "<C-w>h")
+    map("n", "<C-l>", "<C-w>l")
+    map("n", "<C-k>", "<C-w>k")
+    map("n", "<C-j>", "<C-w>j")
+    
 
     -- Keymappings
 
@@ -17,4 +25,25 @@ M.fzf = {
     },
 }
 
+M.lspconfig = function(opts)
+    local map = vim.keymap.set 
+    -- Enable completion triggered by <c-x><c-o>
+    map('n', 'gD', vim.lsp.buf.declaration, opts)
+    map('n', 'gd', vim.lsp.buf.definition, opts)
+    map('n', 'K', vim.lsp.buf.hover, opts)
+    map('n', 'gi', vim.lsp.buf.implementation, opts)
+    map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    map('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    map('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    map('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    map('n', 'gr', vim.lsp.buf.references, opts)
+    map('n', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+end
 return M
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,10 +1,14 @@
 local opt = vim.opt
 
+-- Encoding
+opt.encoding = 'utf-8'
+
+-- Terminal
+opt.termguicolors = true
 
 -- Number column
 opt.relativenumber = true
 opt.numberwidth = 2
-
 
 -- Indentation
 opt.autoindent = true
@@ -13,3 +17,19 @@ opt.expandtab = true
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.shiftround = true
+
+-- Syntax
+opt.syntax = 'on' 
+
+-- Cursor
+opt.cursorline = true
+
+-- Splitting
+opt.splitright = true
+opt.splitbelow = true
+
+-- Ruler
+opt.ruler = true
+
+-- Clipboard
+opt.clipboard = 'unnamedplus'
