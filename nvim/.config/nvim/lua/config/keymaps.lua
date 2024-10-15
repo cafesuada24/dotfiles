0ae8b2d2cffdcf7bfbd811b7e2b8d9123078aa17
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-10-15 12:11:42 +0700

    Add neotree plugin

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -53,4 +53,47 @@ M.lspconfig = function(opts)
     end, opts)
 end
 
+M.trouble = {
+    {
+      '<leader>xx',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xX',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Buffer Diagnostics (Trouble)',
+    },
+    {
+      '<leader>cs',
+      '<cmd>Trouble symbols toggle focus=false<cr>',
+      desc = 'Symbols (Trouble)',
+    },
+    {
+      '<leader>cl',
+      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+      desc = 'LSP Definitions / references / ... (Trouble)',
+    },
+    {
+      '<leader>xL',
+      '<cmd>Trouble loclist toggle<cr>',
+      desc = 'Location List (Trouble)',
+    },
+    {
+      '<leader>xQ',
+      '<cmd>Trouble qflist toggle<cr>',
+      desc = 'Quickfix List (Trouble)',
+    }
+}
+
+M.neo_tree = {
+    {
+        '<tab>',
+        '<cmd>Neotree toggle<CR>',
+        desc = 'Toggle neotree',
+        mode = 'n'
+    }
+}
+
+
 return M
