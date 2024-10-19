984741123f266b3de41f7aa44eac7bf0fa38a291
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-10-19 17:29:56 +0700

    update fzf

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,13 +1,31 @@
 return {
-    dir = '/opt/fzf',
-    -- 'junegunn/fzf',
 
-    build = function()
-        vim.fn['fzf#install']()
-    end,
+    'junegunn/fzf.vim',
+    dependencies = {
+        {
+            dir = '/opt/fzf',
+            build = function()
+                vim.fn['fzf#install']()
+            end,
+        }
+    },
 
-    opts = {},
+
+    -- init = function()
+    --     vim.cmd([[
+    --         let g:fzf_vim = {}
+    --         let g:fzf_vim.preview_window = ['hidden,right,50%,<70(up,40%)', 'ctrl-/']
+    --     ]])
+    -- end,
 
     keys = require('config.keymaps').fzf,
 
+    config = function()
+        local g = vim.g
+        g.fzf_vim = {}
+
+        g.fzf_vim.preview_window = {'up:30%', 'ctrl-/'} 
+    end,
+
+    lazy = false
 }
