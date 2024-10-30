3a658882179285603615d8c6ae3eb12d60bb4c2a
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-10-31 00:16:03 +0700

    Update

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,260 +2,260 @@
 local M = {}
 
 M.init = function()
-    local map = vim.keymap.set
+  local map = vim.keymap.set
 
-    local opts = { noremap = true, silent = true }
+  local opts = { noremap = true, silent = true }
 
-    -- Keymappings
-    map({ 'i', 'n' }, '<C-c>', '<Esc>', opts)
+  -- Keymappings
+  map({ 'i', 'n' }, '<C-c>', '<Esc>', opts)
 
-    -- Clear search with <esc>
-    map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and Clear hlsearch' })
+  -- Clear search with <esc>
+  map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and Clear hlsearch' })
 
-    -- Window Navigation
-    map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
-    map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
-    map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
-    map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
+  -- Window Navigation
+  map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
+  map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
+  map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
+  map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
 
-    -- Resize window using <ctrl> arrow keys
-    map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
-    map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
-    map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
-    map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
+  -- Resize window using <ctrl> arrow keys
+  map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+  map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+  map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+  map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
 
-    -- Buffer
-    map('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
-    map('n', ']b', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+  -- Buffer
+  map('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
+  map('n', ']b', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 
-    -- Windows
-    map('n', '<leader>-', '<C-W>s', { desc = 'Split Window Below', remap = true })
-    map('n', '<leader>|', '<C-W>v', { desc = 'Split Window Right', remap = true })
+  -- Windows
+  map('n', '<leader>-', '<C-W>s', { desc = 'Split Window Below', remap = true })
+  map('n', '<leader>|', '<C-W>v', { desc = 'Split Window Right', remap = true })
 
-    -- better indenting
-    map('v', '<', '<gv')
-    map('v', '>', '>gv')
+  -- better indenting
+  map('v', '<', '<gv')
+  map('v', '>', '>gv')
 
-    -- Tabs
-    map('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First Tab' })
-    map('n', '<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last Tab' })
-    map('n', '<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'New Tab' })
-    map('n', '<leader><tab>[', '<cmd>tabprevious<cr>', { desc = 'Previous Tab' })
-    map('n', '<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Next Tab' })
-    map('n', '<leader><tab>d', '<cmd>tabclose<cr>', { desc = 'Close Tab' })
-    map('n', '<leader><tab>o', '<cmd>tabonly<cr>', { desc = 'Close Other Tabs' })
+  -- Tabs
+  map('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First Tab' })
+  map('n', '<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last Tab' })
+  map('n', '<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'New Tab' })
+  map('n', '<leader><tab>[', '<cmd>tabprevious<cr>', { desc = 'Previous Tab' })
+  map('n', '<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Next Tab' })
+  map('n', '<leader><tab>d', '<cmd>tabclose<cr>', { desc = 'Close Tab' })
+  map('n', '<leader><tab>o', '<cmd>tabonly<cr>', { desc = 'Close Other Tabs' })
 end
 
 M.fzf_lua = {
-    {
-        '<C-p>',
-        '<cmd>lua require("fzf-lua").files()<CR>',
-        silent = true,
-        desc = 'FZF files'
-    },
-    {
-        '<leader>ca',
-        '<cmd>FzfLua lsp_code_actions<cr>',
-        desc = 'Open code action',
-        mode = { 'n', 'v' }
-    },
+  {
+    '<C-p>',
+    '<cmd>lua require("fzf-lua").files()<CR>',
+    silent = true,
+    desc = 'FZF files'
+  },
+  {
+    '<leader>ca',
+    '<cmd>FzfLua lsp_code_actions<cr>',
+    desc = 'Open code action',
+    mode = { 'n', 'v' }
+  },
 
-    -- git
-    { '<leader>gc', '<cmd>FzfLua git_commits<CR>',                                                             desc = 'Commits' },
-    { '<leader>gs', '<cmd>FzfLua git_status<CR>',                                                              desc = 'Status' },
+  -- git
+  { '<leader>gc', '<cmd>FzfLua git_commits<CR>',                                                             desc = 'Commits' },
+  { '<leader>gs', '<cmd>FzfLua git_status<CR>',                                                              desc = 'Status' },
 
-    -- search
-    { '<leader>sd', '<cmd>FzfLua diagnostics_document<cr>',                                                    desc = 'Document Diagnostics' },
-    { '<leader>sq', '<cmd>FzfLua quickfix<cr>',                                                                desc = 'Quickfix List' },
+  -- search
+  { '<leader>sd', '<cmd>FzfLua diagnostics_document<cr>',                                                    desc = 'Document Diagnostics' },
+  { '<leader>sq', '<cmd>FzfLua quickfix<cr>',                                                                desc = 'Quickfix List' },
 
-    -- lsp integration
-    { 'gd',         '<cmd>FzfLua lsp_definitions     jump_to_single_result=true ignore_current_line=true<cr>', desc = 'Goto Definition' },
-    { 'gr',         '<cmd>FzfLua lsp_references      jump_to_single_result=true ignore_current_line=true<cr>', desc = 'References',            nowait = true },
-    { 'gI',         '<cmd>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<cr>', desc = 'Goto Implementation' },
-    { 'gy',         '<cmd>FzfLua lsp_typedefs        jump_to_single_result=true ignore_current_line=true<cr>', desc = 'Goto T[y]pe Definition' },
+  -- lsp integration
+  { 'gd',         '<cmd>FzfLua lsp_definitions     jump_to_single_result=true ignore_current_line=true<cr>', desc = 'Goto Definition' },
+  { 'gr',         '<cmd>FzfLua lsp_references      jump_to_single_result=true ignore_current_line=true<cr>', desc = 'References',            nowait = true },
+  { 'gI',         '<cmd>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<cr>', desc = 'Goto Implementation' },
+  { 'gy',         '<cmd>FzfLua lsp_typedefs        jump_to_single_result=true ignore_current_line=true<cr>', desc = 'Goto T[y]pe Definition' },
 }
 
 M.lspconfig = function(opts)
-    local map = vim.keymap.set
-    -- Enable completion triggered by <c-x><c-o>
-    map('n', 'gD', vim.lsp.buf.declaration, opts)
-    -- map('n', 'gd', vim.lsp.buf.definition, opts)
-    -- map('n', 'gI', vim.lsp.buf.implementation, opts)
-    -- map('n', 'gy', vim.lsp.buf.type_definition, opts)
-    -- map('n', 'gr', vim.lsp.buf.references, opts)
-    map('n', 'K', vim.lsp.buf.hover, opts)
-    map('n', 'gK', vim.lsp.buf.signature_help, opts)
-    map('i', '<C-k>', vim.lsp.buf.signature_help, opts)
-    map('n', '<leader>cr', vim.lsp.buf.rename, opts)
-    -- formatting
-    map({ 'n', 'v' }, '<leader>cf', function()
-        vim.lsp.buf.format({ async = true })
-    end, { desc = 'Format' })
+  local map = vim.keymap.set
+  -- Enable completion triggered by <c-x><c-o>
+  map('n', 'gD', vim.lsp.buf.declaration, opts)
+  -- map('n', 'gd', vim.lsp.buf.definition, opts)
+  -- map('n', 'gI', vim.lsp.buf.implementation, opts)
+  -- map('n', 'gy', vim.lsp.buf.type_definition, opts)
+  -- map('n', 'gr', vim.lsp.buf.references, opts)
+  map('n', 'K', vim.lsp.buf.hover, opts)
+  map('n', 'gK', vim.lsp.buf.signature_help, opts)
+  map('i', '<C-k>', vim.lsp.buf.signature_help, opts)
+  map('n', '<leader>cr', vim.lsp.buf.rename, opts)
+  -- formatting
+  map({ 'n', 'v' }, '<leader>cf', function()
+    vim.lsp.buf.format({ async = true })
+  end, { desc = 'Format' })
 end
 
 M.trouble = {
-    {
-        '<leader>xx',
-        '<cmd>Trouble diagnostics toggle<cr>',
-        desc = 'Diagnostics (Trouble)',
-    },
-    {
-        '<leader>xX',
-        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-        desc = 'Buffer Diagnostics (Trouble)',
-    },
-    {
-        '<leader>cs',
-        '<cmd>Trouble symbols toggle focus=false<cr>',
-        desc = 'Symbols (Trouble)',
-    },
-    {
-        '<leader>cl',
-        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-        desc = 'LSP Definitions / references / ... (Trouble)',
-    },
-    {
-        '<leader>xL',
-        '<cmd>Trouble loclist toggle<cr>',
-        desc = 'Location List (Trouble)',
-    },
-    {
-        '<leader>xQ',
-        '<cmd>Trouble qflist toggle<cr>',
-        desc = 'Quickfix List (Trouble)',
-    },
-    {
-        '[q',
-        function()
-            if require('trouble').is_open() then
-                require('trouble').prev({ skip_groups = true, jump = true })
-            else
-                local ok, err = pcall(vim.cmd.cprev)
-                if not ok then
-                    vim.notify(err, vim.log.levels.ERROR)
-                end
-            end
-        end,
-        desc = 'Previous Trouble/Quickfix Item',
-    },
-    {
-        ']q',
-        function()
-            if require('trouble').is_open() then
-                require('trouble').next({ skip_groups = true, jump = true })
-            else
-                local ok, err = pcall(vim.cmd.cnext)
-                if not ok then
-                    vim.notify(err, vim.log.levels.ERROR)
-                end
-            end
-        end,
-        desc = 'Next Trouble/Quickfix Item',
-    },
+  {
+    '<leader>xx',
+    '<cmd>Trouble diagnostics toggle<cr>',
+    desc = 'Diagnostics (Trouble)',
+  },
+  {
+    '<leader>xX',
+    '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+    desc = 'Buffer Diagnostics (Trouble)',
+  },
+  {
+    '<leader>cs',
+    '<cmd>Trouble symbols toggle focus=false<cr>',
+    desc = 'Symbols (Trouble)',
+  },
+  {
+    '<leader>cl',
+    '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+    desc = 'LSP Definitions / references / ... (Trouble)',
+  },
+  {
+    '<leader>xL',
+    '<cmd>Trouble loclist toggle<cr>',
+    desc = 'Location List (Trouble)',
+  },
+  {
+    '<leader>xQ',
+    '<cmd>Trouble qflist toggle<cr>',
+    desc = 'Quickfix List (Trouble)',
+  },
+  {
+    '[q',
+    function()
+      if require('trouble').is_open() then
+        require('trouble').prev({ skip_groups = true, jump = true })
+      else
+        local ok, err = pcall(vim.cmd.cprev)
+        if not ok then
+          vim.notify(err, vim.log.levels.ERROR)
+        end
+      end
+    end,
+    desc = 'Previous Trouble/Quickfix Item',
+  },
+  {
+    ']q',
+    function()
+      if require('trouble').is_open() then
+        require('trouble').next({ skip_groups = true, jump = true })
+      else
+        local ok, err = pcall(vim.cmd.cnext)
+        if not ok then
+          vim.notify(err, vim.log.levels.ERROR)
+        end
+      end
+    end,
+    desc = 'Next Trouble/Quickfix Item',
+  },
 }
 
 M.neo_tree = {
-    {
-        '<leader>fe',
-        '<cmd>Neotree toggle<CR>',
-        desc = 'Toggle neotree (ROOT Dir)',
-        mode = 'n',
-    },
-    { '<tab>', '<leader>fe', desc = 'Explorer NeoTree (Root Dir)', remap = true },
-    -- { '<leader>E', '<leader>fE', desc = 'Explorer NeoTree (cwd)', remap = true },
-    {
-        '<leader>ge',
-        '<cmd>Neotree toggle source=git_status<cr>',
-        desc = 'Toggle neotree gitstatus',
-        mode = 'n',
-    },
-    {
-        '<leader>be',
-        '<cmd>Neotree toggle source=buffers<cr>',
-        desc = 'Toggle neotree buffers',
-        mode = 'n',
-    },
+  {
+    '<leader>fe',
+    '<cmd>Neotree toggle<CR>',
+    desc = 'Toggle neotree (ROOT Dir)',
+    mode = 'n',
+  },
+  { '<tab>', '<leader>fe', desc = 'Explorer NeoTree (Root Dir)', remap = true },
+  -- { '<leader>E', '<leader>fE', desc = 'Explorer NeoTree (cwd)', remap = true },
+  {
+    '<leader>ge',
+    '<cmd>Neotree toggle source=git_status<cr>',
+    desc = 'Toggle neotree gitstatus',
+    mode = 'n',
+  },
+  {
+    '<leader>be',
+    '<cmd>Neotree toggle source=buffers<cr>',
+    desc = 'Toggle neotree buffers',
+    mode = 'n',
+  },
 }
 
 M.gitsigns = function(gs, bufnr)
-    local function map(mode, l, r, desc)
-        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
-    end
+  local function map(mode, l, r, desc)
+    vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+  end
 
-    map('n', ']h', function()
-        if vim.wo.diff then
-            vim.cmd.normal({ ']c', bang = true })
-        else
-            gs.nav_hunk('next')
-        end
-    end, 'Next Hunk')
-    map('n', '[h', function()
-        if vim.wo.diff then
-            vim.cmd.normal({ '[c', bang = true })
-        else
-            gs.nav_hunk('prev')
-        end
-    end, 'Prev Hunk')
-    map('n', ']H', function() gs.nav_hunk('last') end, 'Last Hunk')
-    map('n', '[H', function() gs.nav_hunk('first') end, 'First Hunk')
-    map({ 'n', 'v' }, '<leader>ghs', ':Gitsigns stage_hunk<CR>', 'Stage Hunk')
-    map({ 'n', 'v' }, '<leader>ghr', ':Gitsigns reset_hunk<CR>', 'Reset Hunk')
-    map('n', '<leader>ghS', gs.stage_buffer, 'Stage Buffer')
-    map('n', '<leader>ghu', gs.undo_stage_hunk, 'Undo Stage Hunk')
-    map('n', '<leader>ghR', gs.reset_buffer, 'Reset Buffer')
-    map('n', '<leader>ghp', gs.preview_hunk_inline, 'Preview Hunk Inline')
-    map('n', '<leader>ghb', function() gs.blame_line({ full = true }) end, 'Blame Line')
-    map('n', '<leader>ghB', function() gs.blame() end, 'Blame Buffer')
-    map('n', '<leader>ghd', gs.diffthis, 'Diff This')
-    map('n', '<leader>ghD', function() gs.diffthis('~') end, 'Diff This ~')
-    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'GitSigns Select Hunk')
+  map('n', ']h', function()
+    if vim.wo.diff then
+      vim.cmd.normal({ ']c', bang = true })
+    else
+      gs.nav_hunk('next')
+    end
+  end, 'Next Hunk')
+  map('n', '[h', function()
+    if vim.wo.diff then
+      vim.cmd.normal({ '[c', bang = true })
+    else
+      gs.nav_hunk('prev')
+    end
+  end, 'Prev Hunk')
+  map('n', ']H', function() gs.nav_hunk('last') end, 'Last Hunk')
+  map('n', '[H', function() gs.nav_hunk('first') end, 'First Hunk')
+  map({ 'n', 'v' }, '<leader>ghs', ':Gitsigns stage_hunk<CR>', 'Stage Hunk')
+  map({ 'n', 'v' }, '<leader>ghr', ':Gitsigns reset_hunk<CR>', 'Reset Hunk')
+  map('n', '<leader>ghS', gs.stage_buffer, 'Stage Buffer')
+  map('n', '<leader>ghu', gs.undo_stage_hunk, 'Undo Stage Hunk')
+  map('n', '<leader>ghR', gs.reset_buffer, 'Reset Buffer')
+  map('n', '<leader>ghp', gs.preview_hunk_inline, 'Preview Hunk Inline')
+  map('n', '<leader>ghb', function() gs.blame_line({ full = true }) end, 'Blame Line')
+  map('n', '<leader>ghB', function() gs.blame() end, 'Blame Buffer')
+  map('n', '<leader>ghd', gs.diffthis, 'Diff This')
+  map('n', '<leader>ghD', function() gs.diffthis('~') end, 'Diff This ~')
+  map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'GitSigns Select Hunk')
 end
 
 M.which_key = {
-    {
-        '<leader>?',
-        function()
-            require('which-key').show({ global = false })
-        end,
-        desc = 'Buffer Local Keymaps (which-key)',
-    },
+  {
+    '<leader>?',
+    function()
+      require('which-key').show({ global = false })
+    end,
+    desc = 'Buffer Local Keymaps (which-key)',
+  },
 }
 
 M.which_key_spec = {
+  {
+    mode = { 'n', 'v' },
+    { '<leader><tab>', group = 'tabs' },
+    { '<leader>c', group = 'code' },
+    { '<leader>f', group = 'file/find' },
+    { '<leader>g', group = 'git' },
+    { '<leader>gh', group = 'hunks' },
+    { '<leader>q', group = 'quit/session' },
+    { '<leader>s', group = 'search' },
+    { '<leader>u', group = 'ui', icon = { icon = '󰙵 ', color = 'cyan' } },
+    { '<leader>x', group = 'diagnostics/quickfix', icon = { icon = '󱖫 ', color = 'green' } },
+    { '[', group = 'prev' },
+    { ']', group = 'next' },
+    { 'g', group = 'goto' },
+    { 'gs', group = 'surround' },
+    { 'z', group = 'fold' },
     {
-        mode = { 'n', 'v' },
-        { '<leader><tab>', group = 'tabs' },
-        { '<leader>c', group = 'code' },
-        { '<leader>f', group = 'file/find' },
-        { '<leader>g', group = 'git' },
-        { '<leader>gh', group = 'hunks' },
-        { '<leader>q', group = 'quit/session' },
-        { '<leader>s', group = 'search' },
-        { '<leader>u', group = 'ui', icon = { icon = '󰙵 ', color = 'cyan' } },
-        { '<leader>x', group = 'diagnostics/quickfix', icon = { icon = '󱖫 ', color = 'green' } },
-        { '[', group = 'prev' },
-        { ']', group = 'next' },
-        { 'g', group = 'goto' },
-        { 'gs', group = 'surround' },
-        { 'z', group = 'fold' },
-        {
-            '<leader>b',
-            group = 'buffer',
-            expand = function()
-                return require('which-key.extras').expand.buf()
-            end,
-        },
-        {
-            '<leader>w',
-            group = 'windows',
-            proxy = '<c-w>',
-            expand = function()
-                return require('which-key.extras').expand.win()
-            end,
-        },
-        -- better descriptions
-        { 'gx', desc = 'Open with system app' },
+      '<leader>b',
+      group = 'buffer',
+      expand = function()
+        return require('which-key.extras').expand.buf()
+      end,
     },
+    {
+      '<leader>w',
+      group = 'windows',
+      proxy = '<c-w>',
+      expand = function()
+        return require('which-key.extras').expand.win()
+      end,
+    },
+    -- better descriptions
+    { 'gx', desc = 'Open with system app' },
+  },
 }
 
 return M
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,5 +1,27 @@
-local opt = vim.opt
 local g = vim.g
+
+g.markdown_recommended_style = 0
+
+-- Providers
+g.python3_host_prog = '/home/serein/miniforge3/envs/nvim/bin/python'
+
+
+local opt = vim.opt
+
+opt.confirm = true
+opt.showmode = false -- We have status line
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+
+-- Menu
+opt.completeopt = 'menu,menuone,noselect'
+
+
+-- Smoothscroll
+opt.smoothscroll = true
+
+
+-- Wrap
+opt.wrap = false
 
 -- Encoding
 opt.encoding = 'utf-8'
@@ -30,6 +52,13 @@ opt.shiftwidth = 4
 opt.tabstop = 4
 opt.shiftround = true
 
+-- Popup
+opt.pumblend = 10
+opt.pumheight = 10
+
+-- Grep
+opt.grepprg = 'rg --vimgrep'
+
 -- Case
 opt.smartcase = true
 
@@ -38,7 +67,7 @@ opt.syntax = 'off'
 opt.showmatch = true
 
 -- Tabcompletion
-opt.wildmode = 'list:longest'
+opt.wildmode = 'longest:full,full'
 
 -- Cursor
 opt.cursorline = true
@@ -46,6 +75,7 @@ opt.cursorline = true
 -- Splitting
 opt.splitright = true
 opt.splitbelow = true
+opt.splitkeep = 'screen'
 
 -- Ruler
 opt.ruler = true
@@ -53,5 +83,3 @@ opt.ruler = true
 -- Clipboard
 opt.clipboard = 'unnamedplus'
 
--- Providers
-g.python3_host_prog = '/home/serein/miniforge3/envs/nvim/bin/python'
