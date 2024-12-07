e7f2dfb2f8c69544982c6e7865a91de4dd7e2e64
Serein <108068915+cafesuada24@users.noreply.github.com>
2024-12-08 00:13:10 +0700

    Minor changes

--- nvim/.config/nvim/lua/plugins/lualine-nvim.lua
+++ nvim/.config/nvim/lua/plugins/lualine-nvim.lua
@@ -36,22 +36,22 @@ return {
     opts.extensions = { 'neo-tree', 'lazy' }
 
 
-    -- Integrate trouble
-    local has_trouble, trouble = pcall(require, 'trouble')
-    if has_trouble then
-      local symbols = trouble.statusline({
-        -- mode = 'lsp_document_symbols',
-        mode = 'symbols',
-        group = {},
-        title = false,
-        filter = { range = true },
-        format = '{kind_icon}{symbol.name:Normal}',
-        hl_group = 'lualine_c_normal'
-      })
-      table.insert(opts.sections.lualine_c, {
-        symbols.get,
-        cond = symbols.has
-      })
-    end
+    -- -- Integrate trouble
+    -- local has_trouble, trouble = pcall(require, 'trouble')
+    -- if has_trouble then
+    --   local symbols = trouble.statusline({
+    --     -- mode = 'lsp_document_symbols',
+    --     mode = 'symbols',
+    --     group = {},
+    --     title = false,
+    --     filter = { range = true },
+    --     format = '{kind_icon}{symbol.name:Normal}',
+    --     hl_group = 'lualine_c_normal'
+    --   })
+    --   table.insert(opts.sections.lualine_c, {
+    --     symbols.get,
+    --     cond = symbols.has
+    --   })
+    -- end
   end,
 }
