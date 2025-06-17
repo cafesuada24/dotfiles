cfb13b7afbf2a4a91cc562997e6d7701a77c4d08
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-06-17 11:14:11 +0700

    Update LSP for clangd

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -119,19 +119,20 @@ return {
           capabilities = {
             offsetEncoding = 'utf-16',
           },
-          -- cmd = {
-          --   'clangd',
-          --   '--background-index',
-          --   '--clang-tidy',
-          --   '--header-insertion=iwyu',
-          --   '--completion-style=detailed',
-          --   '--function-arg-placeholders',
-          --   '--fallback-style=llvm',
-          -- },
+          cmd = {
+            'clangd',
+            '--background-index',
+            '--clang-tidy',
+            '--header-insertion=iwyu',
+            '--completion-style=detailed',
+            '--function-arg-placeholders',
+            -- '--fallback-style=llvm',
+          },
           init_options = {
             usePlaceholders = true,
             completeUnimported = true,
             clangdFileStatus = true,
+            fallbackFlags = { '-std=c++17', '-style=llvm' },
           },
         },
         tsserver = {
