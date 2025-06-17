98d8e588f04889aba61e29e419c8939493ce71bf
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-06-17 11:03:15 +0700

    Update clangd LSP config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -100,6 +100,40 @@ return {
             },
           }
         },
+        clangd = {
+          root_dir = function(fname)
+            local lspconfig_util = require('lspconfig.util')
+            return lspconfig_util.root_pattern(
+              'Makefile',
+              'configure.ac',
+              'configure.in',
+              'config.h.in',
+              'meson.build',
+              'meson_options.txt',
+              'build.ninja'
+            )(fname) or lspconfig_util.root_pattern(
+              'compile_commands.json',
+              'compile_flags.txt'
+            ) or lspconfig_util.find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = 'utf-16',
+          },
+          -- cmd = {
+          --   'clangd',
+          --   '--background-index',
+          --   '--clang-tidy',
+          --   '--header-insertion=iwyu',
+          --   '--completion-style=detailed',
+          --   '--function-arg-placeholders',
+          --   '--fallback-style=llvm',
+          -- },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        },
         tsserver = {
           on_attach = function(client, _)
             client.handlers['textDocument/publishDiagnostics'] = function() end
@@ -108,7 +142,8 @@ return {
           },
         }
       },
-      setup = {},
+      setup = {
+      },
     }
   end,
 
